5bbffba280fa23d53daf12c26e24c8e6c1dffbe0
qqharry21 <qqharry21@gmail.com>
2024-06-27 17:11:52 +0800

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,24 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/lib/i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ['image/webp', 'image/avif'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.wits.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.mitac.com.tw',
+      },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
