be270ad6730030b7010c1b1d01905f5df61bd834
qqharry21 <qqharry21@gmail.com>
2024-07-03 01:32:21 +0800

    feat: add send-message api

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,62 @@
+import { Resend } from 'resend';
+import type { z } from 'zod';
+
+import { NextResponse } from 'next/server';
+
+import { ResponseNoticeEmail, ThankYouEmail } from '@/components/email';
+
+import { connectDB } from '@/lib/mongodb';
+import type { contactSchema } from '@/lib/schemas';
+import { formatDate } from '@/lib/utils';
+
+type ContactFormValues = z.infer<typeof contactSchema>;
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req: Request) {
+  const client = await connectDB();
+  const session = client.startSession();
+  try {
+    session.startTransaction();
+
+    const body = (await req.json()) as ContactFormValues;
+    const currentDate = new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' });
+    const date = formatDate(new Date(), 'PPP iii');
+
+    // Save to database
+    const db = client.db(process.env.MONGODB_DB);
+    await db.collection('contact').insertOne({ ...body, date: currentDate }, { session });
+
+    // Send email
+    const emailTo = [
+      {
+        to: [body.email],
+        react: ThankYouEmail({ ...body, date }),
+        subject: 'Thank you for contacting me',
+      },
+      {
+        to: ['qqharry21@gmail.com'],
+        react: ResponseNoticeEmail({ ...body, date }),
+        subject: `New message from ${body.name}`,
+      },
+    ];
+
+    const mailToSends = emailTo.map((item) => ({
+      ...item,
+      from: 'Harry Chen <harrychen@chenchuanhao.com>',
+      reply_to: 'Harry Chen <qqharry21@gmail.com>',
+      text: 'Hello world',
+    }));
+
+    await resend.batch.send(mailToSends);
+
+    await session.commitTransaction();
+
+    return NextResponse.json({ message: 'Send message successful' }, { status: 200 });
+  } catch (error) {
+    await session.abortTransaction();
+    return new NextResponse('Failed to send message', { status: 500 });
+  } finally {
+    session.endSession();
+  }
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,20 @@
 import type { ClassValue } from 'clsx';
 import { clsx } from 'clsx';
+import type { FormatOptions } from 'date-fns/format';
+import { format } from 'date-fns/format';
+import { zhTW } from 'date-fns/locale/zh-TW';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatDate = (
+  date: Date | number | string,
+  formatString = 'PPPP',
+  options: FormatOptions = { locale: zhTW, weekStartsOn: 1, useAdditionalWeekYearTokens: true }
+) => {
+  return format(date, formatString, {
+    ...options,
+  });
+};
